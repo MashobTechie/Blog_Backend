50a915f8dd00068a2a854f1f621cfddb33fb8a6e
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-08-27 18:24:15 +0100

    Implemented getting posts by ID. Created a middleware function to getPost By ID

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,6 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Posts');
 
+
+
+
+// Middleware Function to get a post by ID (resusable for GET, POST , DELETE)
+
+async function getPost(req, res, next){
+    let post;
+    try {
+        post = await Post.findById(req.params.id);
+        if (post == null) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+    // If a post is found, attach it to the response objectso that the middleware can use it. 
+    res.post = post;
+    next(); // Proceed to the next middleware or route handler
+
+}
+
+
+
 // Route 1: Get all posts
 // GET /posts
 
@@ -40,6 +63,22 @@ router.get('/', async (req, res) => {
           }
     });
 
+
+    // Get a single Post by ID
+    // GET /posts/:id
+    // Route Parameters
+    // (req.params): Express extracts values from the URL path defined with a colon (:) This is how we get the specific ID from the URL
+
+    router.get('/:id', getPost, (req, res) => {
+        try {
+            res.json(res.post); // Send the found post as JSON
+        } catch (error) {
+             res.status(500).json({
+                message: error.message
+             })
+        }
+
+    })
 
 
     module.exports = router;
