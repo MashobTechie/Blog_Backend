6d0d8821a6a99b2bdb8233165e7b40eadf5d97b4
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-09-07 08:04:45 +0100

    get post by id

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -75,7 +75,7 @@ router.get('/', async (req, res) => {
         } catch (error) {
              res.status(500).json({
                 message: error.message
-             })
+             });
         }
 
     })
